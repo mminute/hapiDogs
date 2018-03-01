45f744bbee82aec2ba5244b9fa1c62736dd6902a
Mason Jennings <mjennings@pinterest.com>
2018-02-28 18:59:25 -0800

    Working API!

--- src/models/dog.js
+++ src/models/dog.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose.Schema;
+const { Schema } = mongoose;
 
 const dogModel = new Schema({
   name: { type: String, required: true, index: { unique: true } },
